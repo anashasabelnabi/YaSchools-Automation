5c4f0dde6c5aab6376946f988f9d5bb0171bf272
Anas Hasab Elnabi <anashasabelnabi@outlook.com>
2021-12-15 18:47:37 +0200

    Resolve Warnings

--- src/test/java/tests/RegisterationGridTest.java
+++ src/test/java/tests/RegisterationGridTest.java
@@ -2,12 +2,8 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
